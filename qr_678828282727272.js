c719576670e8b959968e0ae7d80b95a561d7edf9
DanxyPrasetyo <dpras19800@gmail.com>
2025-08-06 12:51:11 +0700

    Add files via upload

---
+++ qr_678828282727272.js
@@ -0,0 +1,17 @@
+{
+  "08983628647": {
+    "izin": true
+  },
+  "KONTOL123": {
+    "izin": false
+  },
+  "BAWOK123": {
+    "izin": false
+  },
+  "PEMBELI001": {
+    "izin": false
+  },
+  "BAJAKAN999": {
+    "izin": false
+  }
+}
